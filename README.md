38b71e15237b9e06a82e0c58536b989e52dd56fa
rgomezh23 <rgomezh23@fpsanturtzilh.eus>
2025-02-13 10:50:22 +0100

    konponentea egin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Erantzun galdera hemen:
+ionic  generate component --spec false
+Gero goiburukoa izena ipintzen duzu eta ondo egindo da
---
+++ src/Konponenteak/goiburukoa/goiburukoa.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goiburukoa',
+  templateUrl: './goiburukoa.component.html',
+  styleUrls: ['./goiburukoa.component.scss'],
+})
+export class GoiburukoaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
